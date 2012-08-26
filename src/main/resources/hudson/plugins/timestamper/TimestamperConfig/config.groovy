aed6c8a6ed836b38a14cff074c254b371a03cbbe
StevenGBrown <stevengbrown@ubuntu-steve.(none)>
2012-08-26 14:04:43 +0800

    Tweak global config messages - Section named after plugin - Fix HTML escaping - Display example timestamp formats in a different font

--- src/main/resources/hudson/plugins/timestamper/TimestamperConfig/config.groovy
+++ src/main/resources/hudson/plugins/timestamper/TimestamperConfig/config.groovy
@@ -2,7 +2,7 @@ package hudson.plugins.timestamper.TimestamperConfig
 
 def f=namespace(lib.FormTagLib)
 
-f.section(title:_("Timestamper Format")) {
+f.section(title:_("Timestamper")) {
     f.entry(title: _("Timestamp format"), field:"timestampFormat") {
         f.textbox()
     }
