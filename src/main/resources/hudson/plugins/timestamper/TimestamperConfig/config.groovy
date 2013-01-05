9e072e8cc59cdf8a9a13e3861a43d57f8ea826b6
StevenGBrown <StevenGBrown@gmail.com>
2013-01-05 10:03:28 +0800

    Update field name in config.groovy to match TimestamperConfig class again

--- src/main/resources/hudson/plugins/timestamper/TimestamperConfig/config.groovy
+++ src/main/resources/hudson/plugins/timestamper/TimestamperConfig/config.groovy
@@ -27,7 +27,7 @@ package hudson.plugins.timestamper.TimestamperConfig
 def f=namespace(lib.FormTagLib)
 
 f.section(title:_("Timestamper")) {
-    f.entry(title: _("System clock time format"), field:"timestampFormat") {
+    f.entry(title: _("System clock time format"), field:"systemTimeFormat") {
         f.textbox()
     }
     f.entry(title: _("Elapsed time format"), field:"elapsedTimeFormat") {
