jenkinsci/timestamper-plugin

commit bfbe4bddcda1de5d3d3b291a488dee77489eeaef

Split new class TimestampsActionQuery out of TimestampsActionOutput

src/main/java/hudson/plugins/timestamper/action/TimestampsAction.java
@@ -26,12 +26,9 @@ package hudson.plugins.timestamper.action;
 import static com.google.common.base.Preconditions.checkNotNull;
 import hudson.model.Action;
 import hudson.model.Run;
-import hudson.plugins.timestamper.io.LogFileReader;
-import hudson.plugins.timestamper.io.TimestampNotesReader;
-import hudson.plugins.timestamper.io.TimestamperPaths;
-import hudson.plugins.timestamper.io.TimestampsFileReader;
-import hudson.plugins.timestamper.io.TimestampsReader;
+import hudson.plugins.timestamper.io.Closeables;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
@@ -39,8 +36,6 @@ import java.util.logging.Logger;
 
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
-
-import com.google.common.base.Optional;
 
 /**
  * Action which serves a page of time-stamps. The format of this page will not
@@ -61,21 +56,13 @@ public final class TimestampsAction implements Action {
   private final Run<?, ?> build;
 
   /**
-   * Generates the page of time-stamps.
-   */
-  private final TimestampsActionOutput output;
-
-  /**
    * Create a {@link TimestampsAction} for the given build.
    * 
    * @param build
    *          the build to inspect
-   * @param output
-   *          generates the page of time-stamps
    */
-  TimestampsAction(Run<?, ?> build, TimestampsActionOutput output) {
+  TimestampsAction(Run<?, ?> build) {
     this.build = checkNotNull(build);
-    this.output = checkNotNull(output);
   }
 
   /**
@@ -113,25 +100,16 @@ public final class TimestampsAction implements Action {
       throws IOException {
     response.setContentType("text/plain;charset=UTF-8");
 
-    TimestampsReader timestampsReader;
-    if (TimestamperPaths.timestampsFile(build).isFile()) {
-      timestampsReader = new TimestampsFileReader(build);
-    } else {
-      timestampsReader = new TimestampNotesReader(build);
-    }
-
-    LogFileReader logFileReader = new LogFileReader(build);
-
     PrintWriter writer = response.getWriter();
+
+    TimestampsActionQuery query = TimestampsActionQuery.create(request
+        .getQueryString());
+    BufferedReader reader = TimestampsActionOutput.open(build, query);
+
     try {
-      output.setQuery(request.getQueryString());
-      while (true) {
-        Optional<String> line = output
-            .nextLine(timestampsReader, logFileReader);
-        if (!line.isPresent()) {
-          break;
-        }
-        writer.println(line.get());
+      String line;
+      while ((line = reader.readLine()) != null) {
+        writer.println(line);
       }
     } catch (Exception e) {
       String urlWithQueryString = request.getRequestURLWithQueryString()
@@ -140,8 +118,7 @@ public final class TimestampsAction implements Action {
       writer.println(e.getClass().getSimpleName() + ": " + e.getMessage());
       LOGGER.log(Level.WARNING, urlWithQueryString, e);
     } finally {
-      timestampsReader.close();
-      logFileReader.close();
+      Closeables.closeQuietly(reader);
       writer.flush();
     }
   }

src/main/java/hudson/plugins/timestamper/action/PrecisionTimestampFormat.java
@@ -1,0 +1,101 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2016 Steven G. Brown
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package hudson.plugins.timestamper.action;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import hudson.plugins.timestamper.Timestamp;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+import com.google.common.base.Function;
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+
+/**
+ * Formats time-stamps as the elapsed time in seconds, with a certain number of
+ * places after decimal point.
+ * 
+ * @author Steven G. Brown
+ */
+final class PrecisionTimestampFormat implements Function<Timestamp, String> {
+
+  /**
+   * The number of places to display after the decimal point.
+   */
+  @Nonnegative
+  private final int precision;
+
+  PrecisionTimestampFormat(int precision) {
+    checkArgument(precision >= 0);
+    this.precision = precision;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String apply(@Nonnull Timestamp timestamp) {
+    long seconds = timestamp.elapsedMillis / 1000;
+    if (precision == 0) {
+      return String.valueOf(seconds);
+    }
+    long millis = timestamp.elapsedMillis % 1000;
+    String fractional = String.format("%03d", millis);
+    if (precision <= 3) {
+      fractional = fractional.substring(0, precision);
+    } else {
+      fractional += Strings.repeat("0", precision - 3);
+    }
+    return String.valueOf(seconds) + "." + fractional;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int hashCode() {
+    return Integer.valueOf(precision).hashCode();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof PrecisionTimestampFormat) {
+      PrecisionTimestampFormat other = (PrecisionTimestampFormat) obj;
+      return precision == other.precision;
+    }
+    return false;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(this).add("precision", precision).toString();
+  }
+}

src/main/java/hudson/plugins/timestamper/action/TimestampsActionFactory.java
@@ -55,7 +55,6 @@ public final class TimestampsActionFactory extends TransientActionFactory<Run> {
    */
   @Override
   public Collection<? extends Action> createFor(Run target) {
-    TimestampsActionOutput output = new TimestampsActionOutput();
-    return Collections.singleton(new TimestampsAction(target, output));
+    return Collections.singleton(new TimestampsAction(target));
   }
 }

src/main/java/hudson/plugins/timestamper/action/TimestampsActionOutput.java
@@ -24,32 +24,28 @@
 package hudson.plugins.timestamper.action;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import hudson.model.Run;
 import hudson.plugins.timestamper.Timestamp;
-import hudson.plugins.timestamper.format.ElapsedTimestampFormat;
-import hudson.plugins.timestamper.format.SystemTimestampFormat;
 import hudson.plugins.timestamper.io.LogFileReader;
+import hudson.plugins.timestamper.io.TimestampNotesReader;
+import hudson.plugins.timestamper.io.TimestamperPaths;
+import hudson.plugins.timestamper.io.TimestampsFileReader;
 import hudson.plugins.timestamper.io.TimestampsReader;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
+import java.io.Reader;
+import java.io.StringReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.TimeZone;
 
-import javax.annotation.CheckForNull;
-import javax.annotation.Nonnull;
-
-import org.apache.commons.lang.LocaleUtils;
 import org.apache.commons.lang.time.DurationFormatUtils;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
-import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableList;
 
 /**
  * Generate a page of time-stamps on behalf of {@link TimestampsAction}.
@@ -86,227 +82,134 @@ import com.google.common.collect.ImmutableList;
  */
 public class TimestampsActionOutput {
 
-  private int startLine;
-
-  private Optional<Integer> endLine;
-
-  private final List<Function<Timestamp, String>> timestampFormats = new ArrayList<Function<Timestamp, String>>();
-
-  private boolean appendLogLine;
-
-  private int linesRead;
-
-  @CheckForNull
-  private Integer cachedLineCount;
-
-  public TimestampsActionOutput() {
-    setQuery(null);
-  }
-
   /**
-   * Set the query string.
+   * Open a reader which provides the page of time-stamps.
    * 
+   * @param build
    * @param query
-   *          the query string
+   * @return a {@link BufferedReader}
    */
-  public void setQuery(String query) {
-    startLine = 0;
-    endLine = Optional.absent();
-    timestampFormats.clear();
-    appendLogLine = false;
+  public static BufferedReader open(Run<?, ?> build, TimestampsActionQuery query) {
+    TimestampsReader timestampsReader;
+    if (TimestamperPaths.timestampsFile(build).isFile()) {
+      timestampsReader = new TimestampsFileReader(build);
+    } else {
+      timestampsReader = new TimestampNotesReader(build);
+    }
 
-    List<QueryParameter> queryParameters = readQueryString(query);
+    LogFileReader logFileReader = new LogFileReader(build);
 
-    Optional<String> timeZoneId = Optional.absent();
-    Locale locale = Locale.getDefault();
-    for (QueryParameter parameter : queryParameters) {
-      if (parameter.name.equalsIgnoreCase("timeZone")) {
-        // '+' was replaced with ' ' by URL decoding, so put it back.
-        timeZoneId = Optional.of(parameter.value.replace("GMT ", "GMT+"));
-      } else if (parameter.name.equalsIgnoreCase("locale")) {
-        locale = LocaleUtils.toLocale(parameter.value);
+    return open(timestampsReader, logFileReader, query);
+  }
+
+  static BufferedReader open(final TimestampsReader timestampsReader,
+      final LogFileReader logFileReader, final TimestampsActionQuery query) {
+    final StringBuilder buffer = new StringBuilder();
+
+    Reader reader = new Reader() {
+      int linesRead;
+      Optional<Integer> endLine = Optional.absent();
+      boolean started;
+
+      @Override
+      public int read(char[] cbuf, int off, int len) throws IOException {
+        if (!started) {
+          LineCountSupplier lineCount = new LineCountSupplier(logFileReader);
+          linesRead = readToStartLine(query, lineCount);
+          endLine = resolveEndLine(query, lineCount);
+          started = true;
+        }
+        while (buffer.length() < len) {
+          Optional<String> nextLine = readNextLine(query);
+          if (!nextLine.isPresent()) {
+            break;
+          }
+          linesRead++;
+          if (endLine.isPresent() && linesRead > endLine.get()) {
+            break;
+          }
+          buffer.append(nextLine.get());
+          buffer.append("\n");
+        }
+        int numRead = new StringReader(buffer.toString()).read(cbuf, off, len);
+        buffer.delete(0, (numRead >= 0 ? numRead : buffer.length()));
+        return numRead;
       }
-    }
 
-    for (QueryParameter parameter : queryParameters) {
-      if (parameter.name.equalsIgnoreCase("time")) {
-        timestampFormats.add(new SystemTimestampFormat(parameter.value,
-            timeZoneId, locale));
-      } else if (parameter.name.equalsIgnoreCase("elapsed")) {
-        timestampFormats.add(new ElapsedTimestampFormat(parameter.value));
-      } else if (parameter.name.equalsIgnoreCase("precision")) {
-        int precision = readPrecision(parameter.value);
-        timestampFormats.add(new PrecisionTimestampFormat(precision));
-      } else if (parameter.name.equalsIgnoreCase("appendLog")) {
-        appendLogLine = (parameter.value.isEmpty() || Boolean
-            .parseBoolean(parameter.value));
-      } else if (parameter.name.equalsIgnoreCase("startLine")) {
-        startLine = Integer.parseInt(parameter.value);
-      } else if (parameter.name.equalsIgnoreCase("endLine")) {
-        endLine = Optional.of(Integer.valueOf(parameter.value));
+      private int readToStartLine(TimestampsActionQuery query,
+          LineCountSupplier lineCount) throws IOException {
+        int linesToSkip = Math.max(query.startLine - 1, 0);
+        if (query.startLine < 0) {
+          linesToSkip = lineCount.get() + query.startLine;
+        }
+
+        for (int line = 0; line < linesToSkip; line++) {
+          timestampsReader.read();
+          logFileReader.nextLine();
+        }
+        return linesToSkip;
       }
-    }
 
-    if (timestampFormats.isEmpty()) {
-      // Default
-      timestampFormats.add(new PrecisionTimestampFormat(3));
-    }
-  }
-
-  private List<QueryParameter> readQueryString(String query) {
-    ImmutableList.Builder<QueryParameter> parameters = new ImmutableList.Builder<QueryParameter>();
-    if (query != null) {
-      String[] pairs = query.split("&");
-      for (String pair : pairs) {
-        String[] nameAndValue = pair.split("=", 2);
-        String name = urlDecode(nameAndValue[0]);
-        String value = (nameAndValue.length == 1 ? ""
-            : urlDecode(nameAndValue[1]));
-        parameters.add(new QueryParameter(name, value));
+      private Optional<Integer> resolveEndLine(TimestampsActionQuery query,
+          LineCountSupplier lineCount) throws IOException {
+        if (query.endLine.isPresent() && query.endLine.get() < 0) {
+          return Optional.of(lineCount.get() + query.endLine.get() + 1);
+        }
+        return query.endLine;
       }
-    }
-    return parameters.build();
-  }
 
-  private String urlDecode(String string) {
-    try {
-      return URLDecoder.decode(string, "UTF-8");
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException(e);
-    }
-  }
+      private Optional<String> readNextLine(TimestampsActionQuery query)
+          throws IOException {
 
-  private int readPrecision(String precision) {
-    if ("seconds".equalsIgnoreCase(precision)) {
-      return 0;
-    }
-    if ("milliseconds".equalsIgnoreCase(precision)) {
-      return 3;
-    }
-    if ("microseconds".equalsIgnoreCase(precision)) {
-      return 6;
-    }
-    if ("nanoseconds".equalsIgnoreCase(precision)) {
-      return 9;
-    }
-    int intPrecision = Integer.parseInt(precision);
-    if (intPrecision < 0) {
-      throw new IllegalArgumentException(
-          "Expected non-negative precision, but was: " + precision);
-    }
-    return intPrecision;
-  }
+        Optional<Timestamp> timestamp = timestampsReader.read();
+        String result = "";
+        if (timestamp.isPresent()) {
+          List<String> parts = new ArrayList<String>();
+          for (Function<Timestamp, String> format : query.timestampFormats) {
+            parts.add(format.apply(timestamp.get()));
+          }
+          result = Joiner.on(' ').join(parts);
+        }
 
-  /**
-   * Generate the next line in the page of time-stamps.
-   * 
-   * @param timestampsReader
-   * @param logFileReader
-   * @return the next line
-   * @throws IOException
-   */
-  public Optional<String> nextLine(TimestampsReader timestampsReader,
-      LogFileReader logFileReader) throws IOException {
+        if (query.appendLogLine) {
+          Optional<String> logFileLine = logFileReader.nextLine();
+          result += " " + logFileLine.or("");
+        }
 
-    linesRead += readToStartLine(timestampsReader, logFileReader);
-    resolveEndLine(logFileReader);
-    if (endLine.isPresent() && linesRead >= endLine.get()) {
-      return Optional.absent();
-    }
-
-    List<String> parts = new ArrayList<String>();
-
-    Optional<Timestamp> timestamp = timestampsReader.read();
-    linesRead++;
-    if (timestamp.isPresent()) {
-      for (Function<Timestamp, String> format : timestampFormats) {
-        parts.add(format.apply(timestamp.get()));
+        if (result.trim().isEmpty()) {
+          return Optional.absent();
+        }
+        return Optional.of(result);
       }
-    }
 
-    if (appendLogLine) {
-      Optional<String> logFileLine = logFileReader.nextLine();
-      if (logFileLine.isPresent()) {
-        parts.add(logFileLine.get());
+      @Override
+      public void close() throws IOException {
+        timestampsReader.close();
+        logFileReader.close();
       }
-    }
+    };
 
-    if (parts.isEmpty()) {
-      return Optional.absent();
-    }
-    return Optional.of(Joiner.on(' ').join(parts));
+    return new BufferedReader(reader);
   }
 
-  private int readToStartLine(TimestampsReader timestampsReader,
-      LogFileReader logFileReader) throws IOException {
-    int linesToSkip = Math.max(startLine - 1, 0);
-    if (startLine < 0) {
-      int lineCount = getLineCount(logFileReader);
-      linesToSkip = lineCount + startLine;
-    }
-    startLine = 0;
+  private static class LineCountSupplier {
 
-    for (int line = 0; line < linesToSkip; line++) {
-      timestampsReader.read();
-      logFileReader.nextLine();
-    }
-    return linesToSkip;
-  }
+    private final LogFileReader logFileReader;
 
-  private void resolveEndLine(LogFileReader logFileReader) throws IOException {
-    if (endLine.isPresent() && endLine.get() < 0) {
-      int lineCount = getLineCount(logFileReader);
-      endLine = Optional.of(lineCount + endLine.get() + 1);
-    }
-  }
+    private Optional<Integer> lineCount = Optional.absent();
 
-  private int getLineCount(LogFileReader logFileReader) throws IOException {
-    if (cachedLineCount == null) {
-      cachedLineCount = logFileReader.lineCount();
-    }
-    return cachedLineCount;
-  }
-
-  private static class QueryParameter {
-
-    final String name;
-
-    final String value;
-
-    QueryParameter(String name, String value) {
-      this.name = checkNotNull(name);
-      this.value = checkNotNull(value);
+    LineCountSupplier(LogFileReader logFileReader) {
+      this.logFileReader = checkNotNull(logFileReader);
     }
 
-    @Override
-    public String toString() {
-      return name + "=" + value;
-    }
-  }
-
-  private static class PrecisionTimestampFormat implements
-      Function<Timestamp, String> {
-
-    private final int precision;
-
-    PrecisionTimestampFormat(int precision) {
-      this.precision = precision;
-    }
-
-    @Override
-    public String apply(@Nonnull Timestamp timestamp) {
-      long seconds = timestamp.elapsedMillis / 1000;
-      if (precision == 0) {
-        return String.valueOf(seconds);
+    int get() throws IOException {
+      if (!lineCount.isPresent()) {
+        lineCount = Optional.of(logFileReader.lineCount());
       }
-      long millis = timestamp.elapsedMillis % 1000;
-      String fractional = String.format("%03d", millis);
-      if (precision <= 3) {
-        fractional = fractional.substring(0, precision);
-      } else {
-        fractional += Strings.repeat("0", precision - 3);
-      }
-      return String.valueOf(seconds) + "." + fractional;
+      return lineCount.get();
     }
+  }
+
+  private TimestampsActionOutput() {
   }
 }

src/main/java/hudson/plugins/timestamper/action/TimestampsActionQuery.java
@@ -1,0 +1,216 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2016 Steven G. Brown
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package hudson.plugins.timestamper.action;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import hudson.plugins.timestamper.Timestamp;
+import hudson.plugins.timestamper.format.ElapsedTimestampFormat;
+import hudson.plugins.timestamper.format.SystemTimestampFormat;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang.LocaleUtils;
+
+import com.google.common.base.Function;
+import com.google.common.base.Objects;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Query for retrieving a page of time-stamps from
+ * {@link TimestampsActionOutput}.
+ * 
+ * @author Steven G. Brown
+ */
+public final class TimestampsActionQuery {
+
+  /**
+   * Create a new {@link TimestampsActionQuery}.
+   * 
+   * @param query
+   *          the query string
+   * @return a new query
+   */
+  public static TimestampsActionQuery create(String query) {
+    int startLine = 0;
+    Optional<Integer> endLine = Optional.absent();
+    List<Function<Timestamp, String>> timestampFormats = new ArrayList<Function<Timestamp, String>>();
+    boolean appendLogLine = false;
+
+    List<QueryParameter> queryParameters = readQueryString(query);
+
+    Optional<String> timeZoneId = Optional.absent();
+    Locale locale = Locale.getDefault();
+    for (QueryParameter parameter : queryParameters) {
+      if (parameter.name.equalsIgnoreCase("timeZone")) {
+        // '+' was replaced with ' ' by URL decoding, so put it back.
+        timeZoneId = Optional.of(parameter.value.replace("GMT ", "GMT+"));
+      } else if (parameter.name.equalsIgnoreCase("locale")) {
+        locale = LocaleUtils.toLocale(parameter.value);
+      }
+    }
+
+    for (QueryParameter parameter : queryParameters) {
+      if (parameter.name.equalsIgnoreCase("time")) {
+        timestampFormats.add(new SystemTimestampFormat(parameter.value,
+            timeZoneId, locale));
+      } else if (parameter.name.equalsIgnoreCase("elapsed")) {
+        timestampFormats.add(new ElapsedTimestampFormat(parameter.value));
+      } else if (parameter.name.equalsIgnoreCase("precision")) {
+        int precision = readPrecision(parameter.value);
+        timestampFormats.add(new PrecisionTimestampFormat(precision));
+      } else if (parameter.name.equalsIgnoreCase("appendLog")) {
+        appendLogLine = (parameter.value.isEmpty() || Boolean
+            .parseBoolean(parameter.value));
+      } else if (parameter.name.equalsIgnoreCase("startLine")) {
+        startLine = Integer.parseInt(parameter.value);
+      } else if (parameter.name.equalsIgnoreCase("endLine")) {
+        endLine = Optional.of(Integer.valueOf(parameter.value));
+      }
+    }
+
+    if (timestampFormats.isEmpty()) {
+      // Default
+      timestampFormats.add(new PrecisionTimestampFormat(3));
+    }
+
+    return new TimestampsActionQuery(startLine, endLine, timestampFormats,
+        appendLogLine);
+  }
+
+  private static List<QueryParameter> readQueryString(String query) {
+    ImmutableList.Builder<QueryParameter> parameters = new ImmutableList.Builder<QueryParameter>();
+    if (query != null) {
+      String[] pairs = query.split("&");
+      for (String pair : pairs) {
+        String[] nameAndValue = pair.split("=", 2);
+        String name = urlDecode(nameAndValue[0]);
+        String value = (nameAndValue.length == 1 ? ""
+            : urlDecode(nameAndValue[1]));
+        parameters.add(new QueryParameter(name, value));
+      }
+    }
+    return parameters.build();
+  }
+
+  private static String urlDecode(String string) {
+    try {
+      return URLDecoder.decode(string, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static int readPrecision(String precision) {
+    if ("seconds".equalsIgnoreCase(precision)) {
+      return 0;
+    }
+    if ("milliseconds".equalsIgnoreCase(precision)) {
+      return 3;
+    }
+    if ("microseconds".equalsIgnoreCase(precision)) {
+      return 6;
+    }
+    if ("nanoseconds".equalsIgnoreCase(precision)) {
+      return 9;
+    }
+    int intPrecision = Integer.parseInt(precision);
+    if (intPrecision < 0) {
+      throw new IllegalArgumentException(
+          "Expected non-negative precision, but was: " + precision);
+    }
+    return intPrecision;
+  }
+
+  private static class QueryParameter {
+
+    final String name;
+
+    final String value;
+
+    QueryParameter(String name, String value) {
+      this.name = checkNotNull(name);
+      this.value = checkNotNull(value);
+    }
+
+    @Override
+    public String toString() {
+      return name + "=" + value;
+    }
+  }
+
+  final int startLine;
+
+  final Optional<Integer> endLine;
+
+  final List<Function<Timestamp, String>> timestampFormats;
+
+  final boolean appendLogLine;
+
+  TimestampsActionQuery(int startLine, Optional<Integer> endLine,
+      List<? extends Function<Timestamp, String>> timestampFormats,
+      boolean appendLogLine) {
+    this.startLine = startLine;
+    this.endLine = checkNotNull(endLine);
+    this.timestampFormats = ImmutableList.copyOf(timestampFormats);
+    this.appendLogLine = appendLogLine;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int hashCode() {
+    return Objects
+        .hashCode(startLine, endLine, timestampFormats, appendLogLine);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof TimestampsActionQuery) {
+      TimestampsActionQuery other = (TimestampsActionQuery) obj;
+      return startLine == other.startLine && endLine.equals(other.endLine)
+          && timestampFormats.equals(other.timestampFormats)
+          && appendLogLine == other.appendLogLine;
+    }
+    return false;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(this).add("startLine", startLine)
+        .add("endLine", endLine).add("timestampFormats", timestampFormats)
+        .add("appendLogLine", appendLogLine).toString();
+  }
+}

commit b95dd34d9b45b587d3dfad1f030e41dd7d25a3ad

Display and log RuntimeException thrown by TimestampsActionQuery.create
This was broken by bfbe4bd.

src/main/java/hudson/plugins/timestamper/action/TimestampsAction.java
@@ -103,15 +103,18 @@ public final class TimestampsAction implements Action {
 
     PrintWriter writer = response.getWriter();
 
-    TimestampsActionQuery query = TimestampsActionQuery.create(request
-        .getQueryString());
+    try {
+      TimestampsActionQuery query = TimestampsActionQuery.create(request
+          .getQueryString()); // throws RuntimeException for invalid query
 
-    try (BufferedReader reader = TimestampsActionOutput.open(build, query)) {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        writer.println(line);
+      try (BufferedReader reader = TimestampsActionOutput.open(build, query)) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          writer.println(line);
+        }
       }
-    } catch (IOException e) {
+
+    } catch (RuntimeException | IOException e) {
       String urlWithQueryString = request.getRequestURLWithQueryString()
           .toString();
       writer.println(urlWithQueryString);