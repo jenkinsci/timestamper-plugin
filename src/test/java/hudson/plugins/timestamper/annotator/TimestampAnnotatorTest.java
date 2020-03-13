fabf67a284ce24dc04c3f8fc1c8a9f247dc4668b
Basil Crow <me@basilcrow.com>
2020-03-13 13:22:56 -0700

    Avoid use of deprecated classes

--- src/test/java/hudson/plugins/timestamper/annotator/TimestampAnnotatorTest.java
+++ src/test/java/hudson/plugins/timestamper/annotator/TimestampAnnotatorTest.java
@@ -25,7 +25,7 @@ package hudson.plugins.timestamper.annotator;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
